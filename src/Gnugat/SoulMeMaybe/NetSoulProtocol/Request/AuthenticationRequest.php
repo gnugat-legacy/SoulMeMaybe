2bb917cf549a7e7c97d8011226b395e3907291f5
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-09-05 13:33:30 +0200

    [Protocol] Adding the authentication request

---
+++ src/Gnugat/SoulMeMaybe/NetSoulProtocol/Request/AuthenticationRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Gnugat\SoulMeMaybe\NetSoulProtocol\Request;
+
+/**
+ * Authentication request class.
+ *
+ * @author Loic Chardonnet <loic.chardonnet@gmail.com>
+ */
+class AuthenticationRequest extends AbstractRequest
+{
+    /**
+     * @var string The command name.
+     */
+    public $commandName = 'ext_user_log';
+
+    /**
+     * @var integer The user login.
+     */
+    public $login;
+
+    /**
+     * @var integer The authentication hash.
+     */
+    public $authenticationHash;
+
+    /**
+     * @var integer The client.
+     */
+    public $client;
+
+    /**
+     * @var integer The user location.
+     */
+    public $userLocation;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRawRequestFromAttribute()
+    {
+        return $this->putsAttributeValuesInRawRequest(array(
+            $this->commandName,
+            $this->login,
+            $this->authenticationHash,
+            $this->client,
+            $this->userLocation,
+        ));
+    }
+}
