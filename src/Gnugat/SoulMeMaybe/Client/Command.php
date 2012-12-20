eaee77d88b2eccd510497c5598563a8e2a0eb7e6
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-12-20 02:16:58 +0100

    [RefactoringCommands] Creating a method to get the dependencies

--- src/Gnugat/SoulMeMaybe/Client/Command.php
+++ src/Gnugat/SoulMeMaybe/Client/Command.php
@@ -49,6 +49,29 @@ EOF
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $dependencies = $this->getDependencies($input, $output);
+
+        $kernel = new Kernel($dependencies['parameters'], $dependencies['output']);
+        $kernel->connect();
+        $kernel->authenticate();
+        $kernel->state();
+        while (true) {
+            sleep(5);
+
+            $kernel->ping();
+        }
+    }
+
+    /**
+     * Gets the dependencies.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface   $input  The input.
+     * @param \Symfony\Component\Console\Output\OutputInterface $output The output.
+     *
+     * @return array The dependencies with their names associated to their values.
+     */
+    private function getDependencies(InputInterface $input, OutputInterface $output)
+    {
         $rootPath = __DIR__.'/../../../..';
 
         $errorHandler = new RotatingFileHandler($rootPath.'/app/logs/errors.txt', 42, Logger::ERROR);
@@ -71,14 +94,9 @@ EOF
 
         $parameters = Yaml::parse($rootPath.'/app/config/parameters.yml');
 
-        $kernel = new Kernel($parameters, $output);
-        $kernel->connect();
-        $kernel->authenticate();
-        $kernel->state();
-        while (true) {
-            sleep(5);
-
-            $kernel->ping();
-        }
+        return array(
+            'output' => $output,
+            'parameters' => $parameters,
+        );
     }
 }
