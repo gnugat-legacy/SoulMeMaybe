3aee4c238ec6d6e70239f317e9cdb3689f4f1c9c
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-12-30 00:53:17 +0100

    [Tests] Testing the run method

---
+++ tests/Gnugat/Tests/Fixtures/PublicOutputCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Gnugat\Tests\Fixtures;
+
+use Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Fixture commands publicing its output.
+ */
+class PublicOutputCommand extends Command
+{
+    /**
+     * @var \Symfony\Component\Console\Output\OutputInterface The output.
+     */
+    public $output;
+
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('fixture:public:input')
+            ->setDescription('Publicing the input')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->output = $output;
+    }
+}
--- tests/Gnugat/Tests/SoulMeMaybe/ApplicationTest.php
+++ tests/Gnugat/Tests/SoulMeMaybe/ApplicationTest.php
@@ -2,7 +2,11 @@
 
 namespace Gnugat\Tests\SoulMeMaybe;
 
-use Gnugat\SoulMeMaybe\Application;
+use Gnugat\SoulMeMaybe\Application,
+    Gnugat\Tests\Fixtures\PublicOutputCommand;
+
+use Symfony\Component\Console\Formatter\OutputFormatterStyle,
+    Symfony\Component\Console\Output\ConsoleOutput;
 
 use PHPUnit_Framework_TestCase;
 
@@ -28,5 +32,29 @@ class ApplicationTest extends PHPUnit_Framework_TestCase
         );
 
         $this->assertEquals($defaultCommands, array_keys($application->all()));
+    }
+
+    public function testRun()
+    {
+        $command = new PublicOutputCommand();
+        $application = new Application();
+        $application->setAutoExit(false);
+
+        $application->add($command);
+        $_SERVER['argv'] = array('cli.php', $command->getName());
+
+        ob_start();
+        $application->run();
+        ob_end_clean();
+
+        $this->assertSame(ConsoleOutput::VERBOSITY_NORMAL, $command->output->getVerbosity());
+
+        $expectedHighlightFormater = new OutputFormatterStyle('red');
+        $actualHighlightFormater = $command->output->getFormatter()->getStyle('highlight');
+        $this->assertSame($expectedHighlightFormater->apply('test'), $actualHighlightFormater->apply('test'));
+
+        $expectedWarningFormater = new OutputFormatterStyle('black', 'yellow');
+        $actualWarningFormater = $command->output->getFormatter()->getStyle('warning');
+        $this->assertSame($expectedWarningFormater->apply('test'), $actualWarningFormater->apply('test'));
     }
 }
