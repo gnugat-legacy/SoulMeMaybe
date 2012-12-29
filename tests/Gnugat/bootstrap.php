9028b51ac4fac23bd3b6a90e59308cd36eec1879
Loic Chardonnet <loic.chardonnet@gmail.com>
2012-12-29 17:23:09 +0100

    [Tests] Creating the bootstrap file

---
+++ tests/Gnugat/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Registering the tests namespace to the autoloader.
+ *
+ * @author Loic Chardonnet <loic.chardonnet@gmail.com>
+ */
+$loader = require __DIR__.'/../../vendor/autoload.php';
+$loader->add('Gnugat\\Tests', __DIR__);
