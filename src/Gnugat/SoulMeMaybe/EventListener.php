98e3254dc51cb4db58a650292d7c6fccffbb9063
Loic Chardonnet <loic.chardonnet@gmail.com>
2013-01-19 13:52:29 +0100

    [2.0.1] Fixing the missing dependencies of Output in EventListener

--- src/Gnugat/SoulMeMaybe/EventListener.php
+++ src/Gnugat/SoulMeMaybe/EventListener.php
@@ -4,6 +4,11 @@ namespace Gnugat\SoulMeMaybe;
 
 use Gnugat\SoulMeMaybe\Configurator\Kernel as Configurator;
 
+use Symfony\Component\Console\Formatter\OutputFormatterStyle,
+    Symfony\Component\Console\Formatter\OutputFormatter,
+    Symfony\Component\Console\Output\ConsoleOutput,
+    Symfony\Component\Console\Helper\DialogHelper;
+
 /**
  * EventListener class.
  *
@@ -16,7 +21,12 @@ class EventListener
      */
     public static function postInstall()
     {
-        $configurator = new Configurator();
+        $styles['highlight'] = new OutputFormatterStyle('red');
+        $styles['warning'] = new OutputFormatterStyle('black', 'yellow');
+        $formatter = new OutputFormatter(null, $styles);
+        $output = new ConsoleOutput(ConsoleOutput::VERBOSITY_NORMAL, null, $formatter);
+
+        $configurator = new Configurator($output, new DialogHelper());
         $configurator->getUserLoginFromCli();
         $configurator->getPasswordSocksFromCli();
         $configurator->writeParametersFile();
