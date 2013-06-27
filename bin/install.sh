0a6a6a7c71debc6b7d8d9aa58537f62a76c5f4a5
Loïc Chardonnet <loic.chardonnet@gmail.com>
2013-06-27 08:41:43 +0200

    [ReDoc] Updated installation documentation
    Fixed typos and removed the 'Setting permissions...' section, as they
    are already set and git keeps them.

--- bin/install.sh
+++ bin/install.sh
@@ -13,16 +13,13 @@ echo 'Installing SoulMeMaybe...'
 git clone https://github.com/gnugat/SoulMeMaybe.git
 cd ./SoulMeMaybe
 
-echo 'Setting rights...'
-chmod +x ./bin/* ./app/console
-
 echo 'Installing composer...'
 curl -s http://getcomposer.org/installer | php
 
 echo 'Installing SoulMeMaybe dependencies...'
 php ./composer.phar install
 
-echo 'Optimising the autoloading...'
+echo 'Optimizing the autoloading...'
 php ./composer.phar dump-autoload -o
 
-echo 'You can run "php ./SoulMeMaybe/app/console" to use it'
+echo 'You can run "cd SoulMeMaybe; app/console" to use it'
